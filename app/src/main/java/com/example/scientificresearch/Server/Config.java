dc86377cda1cc1a70ee3dc3bd39e39da9d083740
nguyendinhphongdx <phongnguyendx@gmail.com>
2021-04-16 18:20:54 +0700

    adđ

--- app/src/main/java/com/example/scientificresearch/Server/Config.java
+++ app/src/main/java/com/example/scientificresearch/Server/Config.java
@@ -2,6 +2,7 @@ package com.example.scientificresearch.Server;
 
 public class Config {
         //public static String url = "http://10.2.32.65:5050";
-       //public static  String url ="https://management--system.herokuapp.com";
-        public static  String url ="http://27.72.28.152:5050";
+        //public static  String url ="https://management--system.herokuapp.com";
+        //public static  String url ="http://27.72.28.152:5050";
+        public static String url = "http://13.59.237.131:5050";
 }
